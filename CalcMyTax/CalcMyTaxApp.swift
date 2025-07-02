7e27865b184afb9f7e945b842a151effe8fb6ee8
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-07-02 14:44:14 -0300

    Initial Commit

---
+++ CalcMyTax/CalcMyTaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcMyTaxApp.swift
+//  CalcMyTax
+//
+//  Created by Rodrigo Cerqueira Reis on 02/07/25.
+//
+
+import SwiftUI
+
+@main
+struct CalcMyTaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
