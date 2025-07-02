0bdc6da26e4f3d43b738d8addf56baa03ae35ff7
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-07-02 15:10:13 -0300

    add the main view

--- CalcMyTax/ContentView.swift
+++ CalcMyTax/ContentView.swift
@@ -8,12 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var salary: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           Text("Annual Salary")
+           TextField("Salary", text: $salary)
+                .frame(width: 200.0, height: 30.0)
+                .keyboardType(.decimalPad)
+                .padding(5)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.black, lineWidth: 1)
+                )
+           
+            Button
+            {} label: {
+                Text("Calculate Tax")
+            }
+            .buttonStyle(.borderedProminent)
         }
         .padding()
     }
