25d8e6a4c059e6d05ecaf5f6f1d60609fc3b9c98
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-07-11 14:57:06 -0300

    add texts and progress views

--- CalcMyTax/ResultsView.swift
+++ CalcMyTax/ResultsView.swift
@@ -13,13 +13,30 @@ struct ResultsView: View {
     var taxBreakdown: [Double] = [5, 10, 15]
     
     var body: some View {
+        VStack {
 
-        PieChartView(data: taxBreakdown,
-                     title: "Tax Detailing",
-                     legend: "Distribution (%)"
-        )
-        .padding()
-        .frame(width: 500, height: 500)
+            PieChartView(data: taxBreakdown,
+                         title: "Tax Detailing",
+                         legend: "Distribution (%)"
+            )
+            Text("Before Tax")
+                .font((.system(size: 32)))
+            Text("$100.000,00")
+            Text("After Tax")
+                .font((.system(size: 32)))
+            Text("$65.000,00")
+                .padding(.bottom)
+            Text("Post Tax Salary")
+            ProgressView("", value: 20, total: 100)
+                .tint(.orange)
+            Text("Tax")
+            ProgressView("", value: 20, total: 100)
+                .tint(.orange)
+            Text("National Insurance")
+            ProgressView("", value: 20, total: 100)
+                .tint(.orange)
+        }
+        .padding(.horizontal)
     }
 }
 
