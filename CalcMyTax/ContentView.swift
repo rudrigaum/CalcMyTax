95e6cabd0639acf1cd051d571fe6ee7195bb1b56
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-07-22 14:48:48 -0300

    add navigation

--- CalcMyTax/ContentView.swift
+++ CalcMyTax/ContentView.swift
@@ -12,25 +12,34 @@ struct ContentView: View {
     @State private var salary: String = ""
     
     var body: some View {
-        VStack {
-           Text("Annual Salary")
-           TextField("Salary", text: $salary)
-                .frame(width: 200.0, height: 30.0)
-                .keyboardType(.decimalPad)
-                .padding(5)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.black, lineWidth: 1)
-                )
-           
-            Button
-            {} label: {
-                Text("Calculate Tax")
+        NavigationView {
+            VStack {
+                Text("Annual Salary")
+                TextField("Salary", text: $salary)
+                    .frame(width: 200.0, height: 30.0)
+                    .keyboardType(.decimalPad)
+                    .padding(5)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.black, lineWidth: 1)
+                    )
+                
+                NavigationLink(destination: ResultsView(), label: {
+                    Text("Calculate Tax")
+                        .bold()
+                        .frame(width: 200, height: 50)
+                        .background(Color.blue)
+                        .foregroundStyle(Color.white)
+                        .cornerRadius(10)
+                })
             }
-            .buttonStyle(.borderedProminent)
+            .padding()
         }
-        .padding()
     }
+}
+
+func goToResultsView() {
+    ResultsView()
 }
 
 #Preview {
